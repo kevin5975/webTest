68fd8afcaf4d427f9507e5f1cee0989609ac67dc
kevin5975 <31097211+kevin5975@users.noreply.github.com>
2019-10-29 09:13:54 +0900

    change by my own
    main.js

--- main.js
+++ main.js
@@ -1,74 +1,61 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
 
+var sendData=0;
+var ack=0;
+var ackack=0;
+var message = "there is no communication now. <br> ";
+
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
+     var _url = request.url;
+     var queryData = url.parse(_url, true).query;
 
-    console.log(queryData );
-
-    if(pathname === '/'){
-      if(queryData.id != null){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
+    if((queryData.dat)*= 1 > 0){
+    sendData =  queryData.dat;
+    message = "sender send data but do not respond now <br>"
     }
-      else{
-        console.log('here?');
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p> this is home page </p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
 
+
+    if(queryData.ack == '1'){
+    ack = queryData.ack;
+    message = "sender sent, and receiver read it <br>"
+    }
+    else{
+      ack = 0;
+    }
+
+    if(queryData.ackack == '1'){
+      ackack = queryData.ackack;
+      message = "receiver responds the answer, and sender read it <br> communication finished ! <br>"
       }
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+      else{
+        ackack = 0;
+      }
 
+    console.log(ack);
 
+    var template = `
+              <!doctype html>
+              <html>
+              <head>
+                <title>TEST</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h2>sendData : ${sendData}</h2>
+                <h2>ack : ${ack} </h2>
+                <h2>ack-ack : ${ackack} </h2>
+
+                <p>I wish this will work well.<br>
+                ${message} </p>
+
+              </body>
+              </html>
+              `;
+              response.writeHead(200);
+              response.end(template);
 
 });
-app.listen(3000);
+
+
+  app.listen(80);
